48a0fe3f315bc2612ad565d9d16b4678e3ba61d6
Fatema04 <shadrinmoni15@gmail.com>
2022-10-22 09:10:01 -0700

    newses added

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsCard from '../../Shared/NewsCard/NewsCard';
 
 const Category = () => {
-    const news = useLoaderData()
+    const categoryNews = useLoaderData()
     return (
         <div>
-            <h2>this  category has {news.length}</h2>
+            <h2>This  category has {categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsCard
+                    key={news._id}
+                    news={news}
+                ></NewsCard>)
+            }
         </div>
     );
 };
--- src/Pages/Shared/NewsCard/NewsCard.js
+++ src/Pages/Shared/NewsCard/NewsCard.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image'
+import { FaRegBookmark, FaShareAlt, FaStar, FaEye } from "react-icons/fa";
 
 
 const NewsCard = ({ news }) => {
-    const { _id, title, author, image_url, details, total_view } = news;
+    const { _id, title, author, image_url, details, rating, total_view } = news;
     return (
-        <Card className="text-center">
-            <Card.Header>Featured</Card.Header>
+        <Card className="mb-3">
+            <Card.Header className='d-flex justify-content-between align-items-center'>
+                <div className='d-flex'>
+                    <Image
+                        roundedCircle
+                        className='me-2'
+                        src={author?.img}
+                        style={{ height: '60px' }}
+                    ></Image>
+                    <div>
+                        <p>{author?.name}</p>
+                        <p>{author?.published_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <FaRegBookmark />
+                    <FaShareAlt />
+                </div>
+            </Card.Header>
             <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img src={image_url} alt="Card image" />
+
                 <Card.Text>
                     {
-                        details.length > 200 ?
+                        details.length > 250 ?
                             <p>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>Read More</Link> </p>
 
                             :
@@ -21,7 +42,16 @@ const NewsCard = ({ news }) => {
                 </Card.Text>
 
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="d-flex justify-content-between align-items-center">
+                <div>
+                    <FaStar className='text-warning me-2' />
+                    <span>{rating?.number}</span>
+                </div>
+                <div>
+                    <FaEye />
+                    <span className='ms-2'>{total_view}</span>
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
