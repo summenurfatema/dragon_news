d3d6483e8caf467deaa8ac6bbfcb1e04ac31a0fd
Fatema04 <shadrinmoni15@gmail.com>
2022-10-21 19:32:02 -0700

    icon added

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 
 const RightSideNav = () => {
     return (
         <div>
             <ButtonGroup vertical>
-                <Button variant='outline-primary'>Login with Google</Button>
-                <Button variant='outline-dark'>Login with Github</Button>
+                <Button variant='outline-primary'><FaGoogle />Login with Google</Button>
+                <Button variant='outline-dark'><FaGithub /> Login with Github</Button>
             </ButtonGroup>
         </div>
     );
