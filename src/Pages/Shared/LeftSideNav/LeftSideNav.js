396d58fa7c45cc28ea9bf5a2eea6f63958ba484b
Fatema04 <shadrinmoni15@gmail.com>
2022-10-21 18:58:33 -0700

    new  setted

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
-            left side nav
+            <h4>All categories</h4>
+            <div>
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
