64050cc560464f9230c8efd46dd4e9bc01d76e33
Fatema04 <shadrinmoni15@gmail.com>
2022-10-22 20:08:25 -0700

    signup form added

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Register = () => {
     return (
-        <div>
-            regint
-        </div>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Register
+            </Button>
+        </Form>
     );
 };
 
