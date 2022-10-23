37974f6c924eff2f99af2be6a493740784191928
Fatema04 <shadrinmoni15@gmail.com>
2022-10-22 20:21:01 -0700

    signup added

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -3,20 +3,28 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const Register = () => {
+    const handleSubmit = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value
+        console.log(name, email, password)
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter name" />
+                <Form.Control type="text" name='name' placeholder="Enter name" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" name='email' placeholder="Enter email" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" name='password' placeholder="Password" />
             </Form.Group>
             <Button variant="primary" type="submit">
                 Register
